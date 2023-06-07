3fc8de48dfe2da42bd838c4f5037198ef9055cf7
Ischi94 <gmathes94@gmx.de>
2023-06-07 11:55:34 +0200

    make own figure 2

--- R/figure_1.R
+++ R/figure_1.R
@@ -10,11 +10,12 @@ dat_clean <- read_rds(here("data",
                            "input",
                            "megafauna_clean.rds"))
 
+data(stages, package = "divDyn")
 
 # overall distribution ----------------------------------------------------
 
 
-plot_1 <- dat_clean %>% 
+plot_1 <- dat_clean %>%
   mutate(log_max = log(max_size_m)) %>% 
   count(group, log_max) %>% 
   mutate(group = factor(group, 
@@ -29,10 +30,12 @@ plot_1 <- dat_clean %>%
   geom_line() +
   geom_point() +
   labs(y = "Taxa count", 
-       x = "Log(Maximum body size [m])") +
+       x = "Maximum body size [m]") +
   scale_colour_brewer(type = "qual", 
                     palette = 2, 
                     name = NULL) +
+  scale_x_continuous(breaks = log(c(1, 2, 5, 10, 20)), 
+                     labels = c(1, 2, 5, 10, 20)) +
   theme_classic(base_size = 12) +
   guides(colour = guide_legend(nrow = 2,
                                byrow = TRUE)) +
@@ -44,26 +47,33 @@ plot_1 <- dat_clean %>%
 
 
 plot_2 <- dat_clean %>%
-  mutate(log_max = log(max_size_m)) %>% 
-  ggplot(aes(age_early_stage, log_max, 
+  mutate(log_max = log(max_size_m), 
+         group = factor(group,
+                        levels = c("Invert",
+                                   "Fish",
+                                   "Chondrichthyes",
+                                   "Reptile",
+                                   "Bird",
+                                   "Mammal"))) %>% 
+  ggplot(aes(xmin = age_early_epoch, 
+             xmax = age_late_epoch, 
+             y = log_max, 
              fill = group)) +
   geom_vline(xintercept = c(443, 365, 252, 
                             210, 66), 
              colour = "grey70", 
              linetype = "dashed") +
-  geom_point(position = position_jitter(width = 0, 
-                                        height = 0.1, 
-                                        seed = 123), 
-             alpha = 0.7, 
-             colour = "grey50", 
-             shape = 21, 
-             size = 2.5) +
-  scale_fill_brewer(type = "qual",
+  geom_linerange(aes(colour = group), 
+                 position = position_dodge2(width = 0.143), 
+                 alpha = 0.8) +
+  scale_colour_brewer(type = "qual",
                     palette = 2,
                     name = NULL) +
-  labs(y = "Log(Maximum body size [m])", 
+  scale_y_continuous(breaks = log(c(1, 2, 5, 10, 20)),
+                     labels = c(1, 2, 5, 10, 20)) +
+  labs(y = "Maximum body size [m]", 
        x = "Age [myr]") +
-  coord_geo(xlim = c(0, 480), 
+  coord_geo(xlim = c(0, 510), 
             dat = list("periods", "eras"),
             pos = list("b", "b"),
             alpha = 0.2, 
@@ -80,69 +90,17 @@ plot_2 <- dat_clean %>%
   theme(legend.position = "none") 
 
 
-
-# ranges ------------------------------------------------------------------
-
-# visualise
-plot_3 <- dat_clean %>% 
-  mutate(taxa = fct_reorder(taxa, age_early_stage)) %>% 
-  mutate(group = factor(group, 
-                        levels = c("Invert", 
-                                   "Fish", 
-                                   "Chondrichthyes", 
-                                   "Reptile", 
-                                   "Bird", 
-                                   "Mammal"))) %>% 
-  ggplot(aes(y = taxa, 
-             colour = group)) +
-  geom_vline(xintercept = c(443, 365, 252, 
-                            210, 66), 
-             colour = "grey70", 
-             linetype = "dashed") +
-  geom_linerange(aes(xmin = age_early_stage, 
-                     xmax = age_late_stage), 
-                 linewidth = 1, 
-                 alpha = 0.5) +
-  labs(y = "Taxon ranges", 
-       x = "Age [myr]") +
-  scale_colour_brewer(type = "qual",
-                      palette = 2,
-                      name = NULL) +
-  coord_geo(xlim = c(0, 480), 
-            dat = list("periods", "eras"),
-            pos = list("b", "b"),
-            alpha = 0.2, 
-            height = unit(0.8, "line"), 
-            size = list(7/.pt, 10/.pt),
-            lab_color = "grey20", 
-            color = "grey20", 
-            abbrv = list(TRUE, FALSE), 
-            fill = "white",
-            expand = TRUE, 
-            lwd = list(0.4, 0.5)) +
-  scale_x_reverse() +
-  theme_classic(base_size = 12) +
-  guides(colour = guide_legend(nrow = 1,
-                               byrow = TRUE)) +
-  theme(legend.position = "none", 
-        axis.text.y = element_blank(), 
-        axis.ticks.y = element_blank())
-
-
-
-
 # patch together ----------------------------------------------------------
 
 
 plot_first <- plot_1 /
   plot_2 / 
-  plot_3 +
   plot_annotation(tag_levels = "A")
 
 # save plot
 ggsave(plot_first, filename = here("figures",
                                   "figure_1.png"), 
-       width = 183, height = 200,
+       width = 183, height = 150,
        units = "mm", 
        bg = "white", device = ragg::agg_png)     
 
