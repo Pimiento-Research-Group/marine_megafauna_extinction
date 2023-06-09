ff0d3bd567dd01e3d1b4fd94f721ec3fbc66f75e
Ischi94 <gmathes94@gmx.de>
2023-06-09 11:29:34 +0200

    add era plot B

--- R/figure_4.R
+++ R/figure_4.R
@@ -14,7 +14,7 @@ dat_clean <- read_rds(here("data",
 # visualise ---------------------------------------------------------------
 
 
-plot_1 <- dat_clean %>% 
+dat_plot_1 <- dat_clean %>% 
   group_by(clade) %>% 
   summarise(max_size = max(max_size_m)) %>% 
   left_join(dat_clean %>% 
@@ -35,11 +35,14 @@ plot_1 <- dat_clean %>%
                                    "Chondrichthyes", 
                                    "Reptile", 
                                    "Bird", 
-                                   "Mammal"))) %>% 
+                                   "Mammal")))
+
+# visualise
+plot_1 <- dat_plot_1 %>%
   ggplot(aes(n, clade)) +
-  # geom_segment(aes(xend = 0,
-  #                  yend = clade),
-  #              colour = "grey70") +
+  geom_segment(aes(xend = 0,
+                   yend = clade),
+               colour = "grey70") +
   geom_point(aes(size = max_size, 
                  fill = group, 
                  colour = late_era), 
@@ -56,17 +59,68 @@ plot_1 <- dat_clean %>%
   guides(fill = guide_legend(override.aes = list(stroke = 0, 
                                                  size = 3.5)), 
          colour = guide_legend(override.aes = list(size = 3))) +
-  scale_size_continuous(breaks = c(1, 10, 20)) +
+  scale_size_continuous(breaks = c(1, 10, 20), 
+                        range = c(1, 6)) +
   scale_color_manual(values = rev(c("#fcea10","#5dc5ea", "#a9c6a9"))) +
   theme_classic(base_size = 12)
   
   
 
+# occurrence per era ------------------------------------------------------
+
+plot_2 <- dat_clean %>%
+  group_by(early_era, clade) %>% 
+  summarise(max_size = max(max_size_m)) %>%
+  ungroup() %>% 
+  complete(clade, early_era, 
+           fill = list(max_size = 0)) %>% 
+  left_join(dat_clean %>% 
+              distinct(group, clade)) %>% 
+  mutate(clade = factor(clade, 
+                        levels = levels(dat_plot_1$clade), 
+                        ordered = TRUE), 
+         group = factor(group, 
+                        levels = c("Invert", 
+                                   "Fish", 
+                                   "Chondrichthyes", 
+                                   "Reptile", 
+                                   "Bird", 
+                                   "Mammal"))) %>%
+  ggplot(aes(early_era, clade,
+             size = max_size, 
+             fill = group, 
+             colour = early_era)) +
+  geom_point(shape = 21, 
+             alpha = 0.8, 
+             stroke = 1) +
+  scale_colour_manual(values = rev(c("#fcea10","#5dc5ea", "#a9c6a9"))) +
+  scale_fill_brewer(type = "qual",
+                    palette = 2,
+                    name = NULL) +
+  scale_size_continuous(range = c(-1, 6)) +
+  labs(x = "Occurrence", 
+       y = NULL) +
+  theme_minimal(base_size = 12) +
+  theme(panel.grid= element_blank(), 
+        axis.text = element_blank(),
+        legend.position = "none")
+
 
 # save --------------------------------------------------------------------
 
+
+# patch together
+plot_final <- plot_1 +
+  plot_2 +
+  plot_layout(widths = c(6, 1), 
+              guides = "collect") +
+  plot_annotation(tag_levels = "A") 
+
+  
+  
+
 # save plot
-ggsave(plot_1, filename = here("figures",
+ggsave(plot_final, filename = here("figures",
                                "figure_4.png"), 
        width = 183, height = 150,
        units = "mm", 
