0a7633ea0ecc23b443b8ca85f5f35fe5279c4b90
Ischi94 <gmathes94@gmx.de>
2024-01-10 10:41:38 +0100

    redo figure 5

--- R/fig_5.R
+++ R/fig_5.R
@@ -7,26 +7,15 @@ library(patchwork)
 # read cleaned data file
 dat_clean <- read_rds(here("data",
                            "input",
-                           "megafauna_clean.rds")) %>% 
-  mutate(across(c(guild, 
-                  vertical, 
-                  habitat), 
-                ~ as.character(.x) %>% 
-                  str_to_sentence())) %>% 
-  mutate(guild = str_replace_all(guild, 
-                                 "Macropredators", 
-                                 "Macropredator"), 
-         guild = str_replace_all(guild, 
-                                 "Generalist", 
-                                 "Macropredator"),
-         vertical = str_replace_all(vertical,
-                                    "Deep-diver",
-                                    "Pelagic"), 
-         vertical = str_replace_all(vertical,
-                                    "Nearshore",
-                                    "Benthopelagic")) 
+                           "megafauna_clean.rds")) %>%
+  # modify
+  mutate(across(c(vertical, habitat, guild),
+                as.character)) %>% 
+  replace_na(list(guild = "Missing",
+                  vertical = "Missing",
+                  habitat = "Missing")) 
 
-data(stages, package = "divDyn")
+
 
 
 
@@ -34,59 +23,118 @@ data(stages, package = "divDyn")
 # visualise ---------------------------------------------------------------
 
 
-# 
-# 
-# plot_eco <- dat_clean %>%
-#   count(early_era, vertical, habitat, guild) %>% 
-#   mutate(across(c(vertical, habitat, guild),
-#                 as.character)) %>% 
-#   replace_na(list(guild = "Missing", 
-#                   vertical = "Missing", 
-#                   habitat = "Missing")) %>% 
-#   mutate(across(c(vertical, habitat, guild), 
-#                 ~ fct_relevel(.x, "Missing",
-#                               after = Inf))) %>% 
-#   ggplot(aes(vertical, habitat, 
-#              shape = guild, 
-#              size = n, 
-#              colour = early_era)) +
-#   geom_point() +
-#   scale_shape_manual(values = c(0, 1, 2, 4)) +
-#   scale_colour_manual(values = rev(c(colorspace::darken("#fcea10", 0.1),
-#                                      "#5dc5ea", "#a9c6a9"))) +
-#   labs(x = NULL, 
-#        y = NULL, 
-#        size = NULL, 
-#        shape = NULL) +
-#   guides(colour = "none",
-#          size = guide_legend(nrow = 1,
-#                              override.aes = list(shape = 3))) +
-#   scale_size_continuous(name = "#Taxa", 
-#                         range = c(1, 9), 
-#                         breaks = c(1, 15, 30)) +
-#   facet_wrap(~ early_era) +
-#   theme_minimal(base_size = 12) +
-#   theme(legend.position = "top", 
-#         legend.box="vertical", 
-#         legend.margin = margin(),
-#         legend.title = element_text(size = 10), 
-#         axis.text.x = element_text(angle = 35,
-#                                    vjust = 0.98,
-#                                    hjust = 0.85), 
-#         axis.text.y = element_text(angle = 35,
-#                                    vjust = 1,
-#                                    hjust = 0.7),
-#         strip.background = element_rect(linewidth = 1), 
-#         panel.grid.major = element_line(colour = "grey95"))
+plot_1 <- dat_clean %>% 
+  mutate(guild = factor(guild, 
+                        levels = c("Macropredator", 
+                                   "Micropredator", 
+                                   "Herbivore", 
+                                   "Missing"))) %>% 
+  ggplot(aes(guild, 
+             fill = group)) +
+  geom_bar() +
+  facet_wrap(~early_era) +
+  labs(y = "Taxon count", 
+       x = NULL, 
+       title = "Guild") +
+  scale_fill_manual(values = c("#1e728eff",
+                                 "#ffbc3cff",
+                                 "darkorange", 
+                                 "coral3",
+                                 "#5d7a64ff",
+                                 "#ad6d8aff",
+                                 "#6d3f2fff",
+                                 "#f9938eff"),
+                      name = NULL) +
+  theme_classic(base_size = 12) +
+  theme(axis.text.x = element_text(size = 10, 
+                                   angle = 20, 
+                                   hjust = 0.5,
+                                   vjust = 0.6), 
+        legend.position = "none", 
+        plot.title = element_text(size = 12),
+        strip.background = element_rect(fill = "grey95", 
+                                        colour = "grey95"))
+
+
+plot_2 <- dat_clean %>%
+  mutate(vertical = factor(vertical,
+                           levels = c("Benthic",
+                                      "Benthopelagic",
+                                      "Pelagic",
+                                      "Missing"))) %>%
+  ggplot(aes(vertical, 
+             fill = group)) +
+  geom_bar() +
+  facet_wrap(~early_era) +
+  labs(y = "Taxon count", 
+       x = NULL, 
+       title = "Vertical position") +
+  scale_fill_manual(values = c("#1e728eff",
+                               "#ffbc3cff",
+                               "darkorange", 
+                               "coral3",
+                               "#5d7a64ff",
+                               "#ad6d8aff",
+                               "#6d3f2fff",
+                               "#f9938eff"),
+                    name = NULL) +
+  theme_classic(base_size = 12) +
+  theme(axis.text.x = element_text(size = 10, 
+                                   angle = 20, 
+                                   hjust = 0.5,
+                                   vjust = 0.6), 
+        legend.position = "none", 
+        plot.title = element_text(size = 12),
+        strip.background = element_rect(fill = "grey95", 
+                                        colour = "grey95"))
+
+plot_3 <- dat_clean %>%
+  mutate(habitat = factor(habitat,
+                          levels = c("Coastal",
+                                     "Coastal/Oceanic",
+                                     "Oceanic",
+                                     "Missing"))) %>%
+  ggplot(aes(habitat, 
+             fill = group)) +
+  geom_bar() +
+  facet_wrap(~early_era) +
+  labs(y = "Taxon count", 
+       x = NULL, 
+       title = "Habitat") +
+  scale_fill_manual(values = c("#1e728eff",
+                               "#ffbc3cff",
+                               "darkorange", 
+                               "coral3",
+                               "#5d7a64ff",
+                               "#ad6d8aff",
+                               "#6d3f2fff",
+                               "#f9938eff"),
+                    name = NULL) +
+  theme_classic(base_size = 12) +
+  theme(axis.text.x = element_text(size = 10, 
+                                   angle = 20, 
+                                   hjust = 0.5,
+                                   vjust = 0.6), 
+        legend.position = "bottom", 
+        plot.title = element_text(size = 12),
+        strip.background = element_rect(fill = "grey95", 
+                                        colour = "grey95"), 
+        legend.key.size = unit(3, "mm"))
+
 
 # save plots --------------------------------------------------------------
 
+# patch together
+plot_eco <- plot_1 / 
+  plot_2 /
+  plot_3 +
+  plot_annotation(tag_levels = "A")
 
 # save plot
 ggsave(plot_eco, 
        filename = here("figures",
                        "figure_5.pdf"), 
-       width = 183, height = 90,
+       width = 183, height = 200,
        units = "mm", 
        bg = "white") 
 
